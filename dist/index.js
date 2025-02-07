2bd7ff97ee27027a892d48a296261188e924d510
Eugene Platonov <eugeneplatonoff@gmail.com>
2025-02-07 16:55:57 +0200

    Upgrade TypeScript to version 5.7.3

--- dist/index.js
+++ dist/index.js
@@ -35652,13 +35652,23 @@ var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (
 }) : function(o, v) {
     o["default"] = v;
 });
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -40496,19 +40506,19 @@ class AzureOpenAI extends OpenAI {
         this.apiVersion = '';
         this._azureADTokenProvider = azureADTokenProvider;
         this.apiVersion = apiVersion;
-        this._deployment = deployment;
+        this.deploymentName = deployment;
     }
-    buildRequest(options) {
+    buildRequest(options, props = {}) {
         if (_deployments_endpoints.has(options.path) && options.method === 'post' && options.body !== undefined) {
             if (!Core.isObj(options.body)) {
                 throw new Error('Expected request body to be an object');
             }
-            const model = this._deployment || options.body['model'];
+            const model = this.deploymentName || options.body['model'];
             if (model !== undefined && !this.baseURL.includes('/deployments')) {
                 options.path = `/deployments/${model}${options.path}`;
             }
         }
-        return super.buildRequest(options);
+        return super.buildRequest(options, props);
     }
     async _getAzureADToken() {
         if (typeof this._azureADTokenProvider === 'function') {
@@ -45982,7 +45992,7 @@ const addFormValue = async (form, key, value) => {
 
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.VERSION = void 0;
-exports.VERSION = '4.79.4'; // x-release-please-version
+exports.VERSION = '4.82.0'; // x-release-please-version
 //# sourceMappingURL=version.js.map
 
 /***/ }),
